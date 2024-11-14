d111b65b91c18fe2ddff8a1107ffc2b6497618c6
dianaemal <146597343+dianaemal@users.noreply.github.com>
2024-11-14 10:49:53 -0800

    Update generate_file.py

--- PL/example2/generate_file.py
+++ PL/example2/generate_file.py
@@ -181,7 +181,8 @@ def process_questions(data, file, info, question_type, html_file=None, py_file=N
                 if key in question:
             # Check if the question has the key and matches one of the values in addDic
                     if question[key] not in values:
-                        print("not creating")
+                        print(question[key])
+                        print(values)
                         matches_all = False  # Found a mismatch
                         break  # No need to check further for this question
 
