a06ef7afbd59de2b09851909d997227f8705a684
dianaemal <146597343+dianaemal@users.noreply.github.com>
2024-11-16 21:38:39 -0800

    Update generate_file.py

--- PL/example2/generate_file.py
+++ PL/example2/generate_file.py
@@ -180,7 +180,9 @@ def process_questions(data, file, info, question_type, html_file=None, py_file=N
             for key, values in addDic.items():
                 if key in question:
             # Check if the question has the key and matches one of the values in addDic
+                    
                     if question[key] not in values:
+                        print(question)
                         print(question[key])
                         print(values)
                         matches_all = False  # Found a mismatch
