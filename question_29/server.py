116707ce78c1df5a5cda23d8d83fa32bbae916b6
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2025-02-10 00:28:26 -0800

    removed old questions -- question#

--- question_29/server.py
+++
@@ -1,6 +0,0 @@
-
-def generate(data):
-    ans = "<p>Tom & Jerry</p>"
-    data["correct_answers"]["ans4"] = ans
-
-    return data
--- question_58/server.py
+++
@@ -1,6 +0,0 @@
-
-def generate(data):
-    ans = "<code><div style="background-color:yellow;"></div></code>"
-    data["correct_answers"]["ans4"] = ans
-
-    return data
