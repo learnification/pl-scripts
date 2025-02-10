c166bcb14c371e5beb82e9ccee97933e71052871
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2025-02-10 01:54:00 -0800

    added updated questions

--- question_50/server.py
+++ question_50/server.py
@@ -4,10 +4,10 @@ def generate(data):
     QUESTION1 = "HTML1"
 
     data["params"][QUESTION1] = [
-        {"tag": "true", "ans": "rgb(255, 0, 0)"},
-        {"tag": "false", "ans": "hsl(120, 100%, 50%)"},
-        {"tag": "false", "ans": "#0000ff"},
-        {"tag": "false", "ans": "None of the options"}
+        {"tag": "true", "ans": "<code>rgb(255, 0, 0)</code>"},
+        {"tag": "false", "ans": "<code>hsl(120, 100%, 50%)</code>"},
+        {"tag": "false", "ans": "<code>#0000ff</code>"},
+        {"tag": "false", "ans": "<code>None of the options</code>"}
     ]
 
     return data
