4ace2fbf997335f7cd15d393ead7d2e02cff44e7
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2025-02-10 00:35:27 -0800

    deleted all question

--- question16/server.py
+++
@@ -1,6 +0,0 @@
-
-def generate(data):
-    ans = "<h1> This is a heading </h1>"
-    data["correct_answers"]["ans4"] = ans
-
-    return data
--- question28/server.py
+++
@@ -1,6 +0,0 @@
-
-def generate(data):
-    ans = "<img src="cat.jpg" alt="Orange cat drinking water">"
-    data["correct_answers"]["ans4"] = ans
-
-    return data
